313dffb3ebe1767aad4918190d787334037a0cb6
Gary James <gary.james19@gmail.com>
2022-10-01 21:44:08 +0100

    Update streams.py

--- tap_googleads/streams.py
+++ tap_googleads/streams.py
@@ -84,7 +84,7 @@ class CustomerHierarchyStream(GoogleAdsStream):
 
     records_jsonpath = "$.results[*]"
     name = "stream_customer_hierarchy"
-    primary_keys = ["customer_client__id"]
+    primary_keys = ["id"]
     replication_key = None
     parent_stream_type = AccessibleCustomers
     schema = th.PropertiesList(
